dae908fc2f332a68559d5bbd15cdb4bfc8c3b2ea
WildWolf <magnus1997.mho@gmail.com>
2021-09-27 20:25:04 +0200

    add db context temp file

---
+++ Models/ModelSaberDbContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ModelSaber.Main.Models
+{
+    public class ModelSaberDbContext
+    {
+        //database model will be added later
+    }
+}
