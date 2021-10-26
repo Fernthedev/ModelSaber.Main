bd7ad6eb9c2544202d291745c2a098db14a9d304
WildWolf <magnus1997.mho@gmail.com>
2021-10-26 23:04:40 +0200

    GraphQL fuckery

--- Models/Enums.cs
+++ Models/Enums.cs
@@ -1,20 +1,29 @@
 ﻿using System;
+using System.ComponentModel;
 
 namespace ModelSaber.Database.Models
 {
+    [Description("Platform type")]
     public enum Platform : byte
     {
+        [Description("Platform PC")]
         Pc,
+        [Description("Platform Quest")]
         Quest
     }
 
-    [Flags]
+    [Flags, Description("Model status")]
     public enum Status : byte
     {
+        [Description("Unpublished")]
         Unpublished = 0,
+        [Description("Published")]
         Published = 1,
+        [Description("ApprovalRequired")]
         ApprovalRequired = 2,
+        [Description("Approved")]
         Approved = 4,
+        [Description("Featured")]
         Featured = 8
     }
 
--- Models/ModelType.cs
+++ Models/ModelType.cs
@@ -1,21 +1,42 @@
-﻿using GraphQL.Types;
+﻿using System.Linq;
+using GraphQL.Types;
+using Microsoft.EntityFrameworkCore;
 
 namespace ModelSaber.Database.Models
 {
-    public class ModelType : ObjectGraphType<Model>
+    public sealed class ModelType : ObjectGraphType<Model>
     {
         public ModelType()
         {
-            Field(o => o.Date, type: typeof(DateGraphType));
-            Field(o => o.Hash);
+            Field(o => o.Date, type: typeof(DateTimeGraphType));
+            Field(o => o.Hash, type: typeof(StringGraphType));
             Field(o => o.Id);
             Field(o => o.Name);
-            Field(o => o.Platform, type: typeof(ByteGraphType));
-            Field(o => o.Status, type: typeof(ByteGraphType));
+            Field(o => o.Platform, type: typeof(PlatformType));
+            Field(o => o.Status, type: typeof(StatusType));
             Field(o => o.Thumbnail);
             Field(o => o.Uuid);
             Field(o => o.DownloadPath);
             Field(o => o.UserId);
+            Field<ListGraphType<TagType>>("tags", resolve: context => context.Source.Tags.Select(t => t.Tag));
         }
+    }
+
+    public class TagType : ObjectGraphType<Tag>
+    {
+        public TagType()
+        {
+            Field(o => o.Id);
+            Field(o => o.Name);
+            Field<ListGraphType<ModelType>>("models", resolve: context => context.Source.ModelTags.Select(t => t.Model));
+        }
+    }
+
+    public class PlatformType : EnumerationGraphType<Platform>
+    {
+    }
+
+    public class StatusType : EnumerationGraphType<Status>
+    {
     }
 }
