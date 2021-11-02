269b8fa28b5abc759e7a162f585babb9c1905e64
WildWolf <magnus1997.mho@gmail.com>
2021-11-02 14:38:45 +0100

    fix alt name not showing

--- ClientApp/src/NavBar.tsx
+++ ClientApp/src/NavBar.tsx
@@ -1,10 +1,10 @@
 ﻿import React, { Component } from "react";
 
-export default class NavBar extends Component{
+export default class NavBar extends Component {
     render() {
         return <header className="navbar sticky-top bg-dark">
             <div className="container-fluid">
-                <a className="btn" href="https://localhost:44385/">
+                <a className="btn btn-dark" href="https://localhost:44385/">
                     <img src="modelsaber-logo-web.svg" alt="ModelSaberImage" className="img-fluid" width="52" height="52" />
                 </a>
                 <div>
