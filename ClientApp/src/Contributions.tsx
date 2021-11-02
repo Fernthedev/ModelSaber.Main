71593574a8e9c33f3cd9160c02fcdfd59c71ef74
Raine <64136899+dawnvt@users.noreply.github.com>
2021-11-02 14:32:49 +0100

    added resources and changes to Contributions

--- ClientApp/src/Contributions.tsx
+++ ClientApp/src/Contributions.tsx
@@ -1,5 +1,7 @@
 ﻿import React, { Component } from "react";
 
+var wild = ["Backend", "Database", "GraphQL", "Pagination"]
+
 export default class Contributions extends Component {
     render(){
         return(
@@ -7,42 +9,74 @@ export default class Contributions extends Component {
                 <h1 className="ms-3 me-3">
                     Current Contributors
                 </h1>
-                <div className="card bg-dark m-1">
-                    <div className="card-header">
-                        <img src="cover.jpg" alt="Contributor 1 Image" className="card-img-top" style={{height:128,width:128}} />
-                        <h2>Contributor 1</h2>
-                        <p className="card-text">
-                            Lorem ipsum dolor sit amet,
-                            consectetur adipiscing elit,
-                            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                        </p>            
-                        <h3>
-                            Support them!
-                        </h3>
-                        <a href="https://github.com/sponsors/legoandmars" className="btn btn-outline-primary">
-                            GitHub Sponsors
-                        </a>
+                <div className="d-inline-flex">
+                    <div className="bg-dark m-1">
+                        <div className="card-header mt-2 mb-2" style={{width:"100%",height:300}}>
+                            <div className="flex-wrap m-2 ms-1">
+                                <img src="raine.png" alt="Image of Raine" style={{height:128,width:128}} />
+                                <h2>Raine</h2>
+                                <p className="card-text">
+                                    Lorem ipsum dolor sit amet,
+                                    consectetur adipiscing elit,
+                                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                                   
+                                </p>            
+                                <h3>
+                                    Support them!
+                                </h3>
+                                <a href="https://github.com/sponsors/dawnvt" className="btn btn-outline-primary">
+                                    GitHub Sponsors
+                                </a>
+                            </div>
+                        </div>
                     </div>
-                </div>
-                <div className="card bg-dark m-1">
-                    <div className="card-header">
-                        <img src="alice.png" alt="Contributor 2 Image" className="card-img-top" style={{height:128,width:128}} />
-                        <h2>Contributor 2</h2>
-                        <p className="card-text">
-                            Lorem ipsum dolor sit amet,
-                            consectetur adipiscing elit,
-                            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                        </p>
-                        <h3>
-                            Support them!
-                        </h3>
-                        <a href="https://github.com/sponsors/legoandmars" className="btn btn-outline-primary">
-                            GitHub Sponsors
-                        </a>
+                    <div className="bg-dark m-1">
+                        <div className="card-header mt-2 mb-2">
+                            <div className="flex-wrap m-2 m-1">
+                                <img src="wildwolf.png" alt="Image of WildWolf" style={{height:128,width:128}} />
+                                <h2>WildWolf</h2>
+                                <p>
+                                    Lorem ipsum dolor sit amet,
+                                    consectetur adipiscing elit,
+                                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                                    
+                                </p>
+                            </div>
+                            
+                            <h3>
+                                Support them!
+                            </h3>
+                            <a href="https://github.com/sponsors/wolfcomp" className="btn btn-outline-primary">
+                                GitHub Sponsors
+                            </a>
+                        </div>
+                    </div>
+                    <div className="bg-dark m-1">
+                        <div className="card-header mt-2 mb-2">
+                            <div className="flex-wrap m-2 m-1">
+                                <img src="omurky.png" alt="Image of WildWolf" style={{height:128,width:128}} />
+                                <h2>Omurky</h2>
+                                <p>
+                                    Lorem ipsum dolor sit amet,
+                                    consectetur adipiscing elit,
+                                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+
+                                </p>
+                            </div>
+
+                            <h3>
+                                Support them!
+                            </h3>
+                            <a href="https://github.com/sponsors/wolfcomp" className="btn btn-outline-primary">
+                                GitHub Sponsors
+                            </a>
+                        </div>
                     </div>
                 </div>
             </div>
