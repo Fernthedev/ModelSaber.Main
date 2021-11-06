eb932c90cb3f99f33b243ac5699fa44fd843736a
WildWolf <magnus1997.mho@gmail.com>
2021-11-06 20:45:02 +0100

    remove old import of style

--- ClientApp/src/App.tsx
+++ ClientApp/src/App.tsx
@@ -5,7 +5,6 @@ import Layout from "./Layout";
 import Developer from "./Developers";
 import Contributions from "./Contributions";
 import Home from "./Home";
-import "bootstrap/dist/css/bootstrap.css"; //this will be later moved to scss file but for now its here so i can debug stuff before i start with custom scss files
 
 export default class App extends Component {
     render() {
