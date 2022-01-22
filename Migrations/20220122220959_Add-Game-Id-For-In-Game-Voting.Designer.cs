e5fc2b7549c3848fbf14ce4ac5e2eff6c2b9ca65
WildWolf <magnus1997.mho@gmail.com>
2022-01-23 00:32:37 +0100

    Add game id to database migrations

--- Migrations/20220122220959_Add-Game-Id-For-In-Game-Voting.Designer.cs
+++ Migrations/20220122220959_Add-Game-Id-For-In-Game-Voting.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ModelSaber.Database;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
@@ -11,9 +12,10 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace ModelSaber.Main.Migrations
 {
     [DbContext(typeof(ModelSaberDbContext))]
-    partial class ModelSaberDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220122220959_Add-Game-Id-For-In-Game-Voting")]
+    partial class AddGameIdForInGameVoting
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20220122220959_Add-Game-Id-For-In-Game-Voting.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ModelSaber.Main.Migrations
+{
+    public partial class AddGameIdForInGameVoting : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "game_id",
+                table: "votes",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "name",
+                table: "models",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "game_id",
+                table: "votes");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "name",
+                table: "models",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+    }
+}
