544d9196ff51a47e1e156e8f2cbf7e50a7fabc92
FernTheDev <15272073+Fernthedev@users.noreply.github.com>
2022-05-06 20:58:19 -0400

    Functional components🦀

--- ClientApp/src/index.tsx
+++ ClientApp/src/index.tsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client';
 
 import "./index.scss";
 import "bootstrap-icons/font/bootstrap-icons.scss";
-import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
@@ -14,8 +13,9 @@ const baseUrl = document.getElementsByTagName("base")[0].getAttribute("href");
 const rootElement = document.getElementById("root");
 const loaderbackground = document.getElementById("lds-roller");
 const uri = process.env.NODE_ENV == "development" ? process.env.REACT_APP_API_URL : "https://apimodelsaber.rainemods.io";
-class Index extends Component {
-    componentDidMount() {
+
+function Index() {
+    useEffect(() => {
         if (window.location.pathname !== "/login") {
             rootElement.classList.remove("loading-hidden");
             loaderbackground.classList.add("loading-hidden");
@@ -23,21 +23,19 @@ class Index extends Component {
                 loaderbackground.classList.add("loading-inactive");
             }, 1000);
         }
-    }
+    }, []);
 
-    render() {
-        return (
-            <React.StrictMode>
-                <Suspense fallback={<Loader />}>
-                    <BrowserRouter basename={baseUrl}>
-                        <GQLClient uri={uri}>
-                            <App />
-                        </GQLClient>
-                    </BrowserRouter>
-                </Suspense>
-            </React.StrictMode>
-        );
-    }
+    return (
+        <React.StrictMode>
+            <Suspense fallback={<Loader />}>
+                <BrowserRouter basename={baseUrl}>
+                    <GQLClient uri={uri}>
+                        <App />
+                    </GQLClient>
+                </BrowserRouter>
+            </Suspense>
+        </React.StrictMode>
+    );
 }
 
 
